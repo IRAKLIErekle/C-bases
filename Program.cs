ef1fedd910085379cd0b435031f4158eee193f90
IRAKLIErekle <irakliphatsia8@gmail.com>
2024-11-11 20:49:35 +0400

    .
    .

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("what is ur name ?");
+Console.ReadLine();
+Console.WriteLine("what is ur age ?");
+Console.ReadLine();
+Console.WriteLine("finished");
